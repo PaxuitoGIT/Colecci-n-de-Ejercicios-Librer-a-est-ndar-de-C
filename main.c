b0c69a2b7a669159c9573442d1377b6b23ab2405
PaxuitoGIT <bxu@myuax.com>
2023-12-18 19:41:09 +0100

    numero aleatorio

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 int main() {
     int num = -10;
@@ -7,6 +8,11 @@ int main() {
     int abs_num = abs(num);
 
     printf("El valor absoluto de %d es %d\n", num, abs_num);
+
+    srand(time(NULL));
+
+    int random_number = rand() % 100;
+    printf("El numero aleatorio es %d\n", random_number);
 
     return 0;
 }
